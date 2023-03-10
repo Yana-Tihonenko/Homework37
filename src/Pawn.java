22b286885a3ffad9ceae315745bc815d369886bd
Yana Tykhonenko <tihonenkoyana@gmail.com>
2023-03-10 17:22:49 +0100

    Change modification  checkCoordinates() method

--- src/Pawn.java
+++ src/Pawn.java
@@ -56,14 +56,13 @@ public class Pawn {
     }
 
 
-    private static void checkCoordinates(int row, int column) {
+    public static void checkCoordinates(int row, int column) {
         if (row < 1 || row > 8) {
             throw new IllegalArgumentException("Некорректный номер строки: " + row);
         }
 
         if (column < 1 || column > 8) {
-            System.out.println("Некорректный номер столбца: " + column);
-            throw new IllegalArgumentException();
+            System.out.println("Некорректный номер столбца: " + column);throw new IllegalArgumentException();
         }
     }
 
